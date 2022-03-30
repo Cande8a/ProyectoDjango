d7f8bd85112f1ac18c09862093e27d6deb74a92c
Cande8a <mc.ochoaromero@gmail.com>
2022-03-30 02:34:42 -0300

    Tienda Online

---
+++ TiendaOnline/gestionPedidos/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from gestionPedidos.models import Articulos
+
+# Create your views here.
+def busqueda_productos(request):
+    return render(request, "busqueda_productos.html")
+
+def buscar(request):
+
+    if request.GET["prd"]:
+        #mensaje="Artículo buscado: %r" %request.GET["prd"]
+        producto = request.GET["prd"]
+        if len(producto)>20:
+            mensaje="texto largo"
+        else:
+            articulos = Articulos.objects.filter(nombre__icontains = producto)
+            return render(request, "resultados_busqueda.html", {"articulos":articulos, "query":producto})
+    else:
+        mensaje="No haz introducido nada"
+    
+    return HttpResponse(mensaje)
+
+def contacto(request):
+    if request.method=="POST":
+        return render(request, "gracias.html")
+    return render(request, "contacto.html")
